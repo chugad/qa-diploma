f69191b4e2f765f93ec592435d0e93f35684abef
Oleg Dudin <o.v.dudin@yandex.ru>
2021-03-09 20:48:06 +0700

    refactoring code

--- src/test/java/ru/netology/data/SQLDataGenerator.java
+++ src/test/java/ru/netology/data/SQLDataGenerator.java
@@ -2,12 +2,32 @@ package ru.netology.data;
 
 import java.sql.*;
 
+import static java.lang.System.*;
+
 public class SQLDataGenerator {
+
+    public static void cleanTables() throws SQLException {
+        String deleteOrderEntity = "delete from order_entity;";
+        String deletePaymentEntity = "delete from payment_entity;";
+        String deleteCreditEntity = "delete from credit_request_entity;";
+
+        try (
+                Connection connectionMysql = DriverManager.getConnection(getUrl(), getUser(), getPassword());
+
+                PreparedStatement statementOrderEntity = connectionMysql.prepareStatement(deleteOrderEntity);
+                PreparedStatement statementPaymentEntity = connectionMysql.prepareStatement(deletePaymentEntity);
+                PreparedStatement statementCreditEntity = connectionMysql.prepareStatement(deleteCreditEntity);
+        ) {
+            statementOrderEntity.executeUpdate();
+            statementPaymentEntity.executeUpdate();
+            statementCreditEntity.executeUpdate();
+        }
+    }
 
     public static boolean approvedPay() {
         boolean result = false;
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
+            Connection connection = DriverManager.getConnection(getUrl(), getUser(), getPassword());
             PreparedStatement stmt = connection.prepareStatement("select oe.payment_id from order_entity as oe\n" +
                     "left join payment_entity as pe on oe.payment_id = pe.transaction_id\n" +
                     "where pe.status = 'APPROVED';");
@@ -20,26 +40,10 @@ public class SQLDataGenerator {
         return result;
     }
 
-//    public static boolean approvedPayFake() {
-//        boolean result = false;
-//        try {
-//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
-//            PreparedStatement stmt = connection.prepareStatement("select oe.payment_id from order_entity as oe\n" +
-//                    "left join credit_request_entity as pe on oe.payment_id = pe.bank_id\n" +
-//                    "where pe.status = 'APPROVED';");
-//            ResultSet rs = stmt.executeQuery();
-//            result = rs.next();
-//
-//        } catch(SQLException exception) {
-//            exception.getErrorCode();
-//        }
-//        return result;
-//    }
-
     public static boolean declinedPay() {
         boolean result = false;
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
+            Connection connection = DriverManager.getConnection(getUrl(), getUser(), getPassword());
             PreparedStatement stmt = connection.prepareStatement("select oe.payment_id from order_entity as oe\n" +
                     "left join payment_entity as pe on oe.payment_id = pe.transaction_id\n" +
                     "where pe.status = 'DECLINED';");
@@ -55,7 +59,7 @@ public class SQLDataGenerator {
     public static boolean approvedCredit() {
         boolean result = false;
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
+            Connection connection = DriverManager.getConnection(getUrl(), getUser(), getPassword());
             PreparedStatement stmt = connection.prepareStatement("select oe.credit_id from order_entity as oe\n" +
                     "left join credit_request_entity as cre on oe.payment_id = cre.bank_id\n" +
                     "where pe.status = 'APPROVED';");
@@ -71,7 +75,7 @@ public class SQLDataGenerator {
     public static boolean declinedCredit() {
         boolean result = false;
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
+            Connection connection = DriverManager.getConnection(getUrl(), getUser(), getPassword());
             PreparedStatement stmt = connection.prepareStatement("select oe.credit_id from order_entity as oe\n" +
                     "left join credit_request_entity as cre on oe.payment_id = cre.bank_id\n" +
                     "where pe.status = 'DECLINED';");
@@ -84,21 +88,15 @@ public class SQLDataGenerator {
         return result;
     }
 
-    public static void cleanTables() throws SQLException {
-        String deleteOrderEntity = "delete from order_entity;";
-        String deletePaymentEntity = "delete from payment_entity;";
-        String deleteCreditEntity = "delete from credit_request_entity;";
+    private static String getUrl() {
+        return System.getProperty("test.db.url");
+    }
 
-        try (
-                Connection connection1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
+    private static String getUser() {
+        return "app";
+    }
 
-                PreparedStatement statementOrderEntity = connection1.prepareStatement(deleteOrderEntity);
-                PreparedStatement statementPaymentEntity = connection1.prepareStatement(deletePaymentEntity);
-                PreparedStatement statementCreditEntity = connection1.prepareStatement(deleteCreditEntity);
-        ) {
-            statementOrderEntity.executeUpdate();
-            statementPaymentEntity.executeUpdate();
-            statementCreditEntity.executeUpdate();
-        }
+    private static String getPassword() {
+        return "pass";
     }
 }
